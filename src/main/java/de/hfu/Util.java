38817c8b8b12ca61e926decb85f43bf53eaea3f1
Maximilian Maes <maximilian.maes@aesculap.de>
2021-11-25 10:33:48 +0100

    Added queue and util class

---
+++ src/main/java/de/hfu/Util.java
@@ -0,0 +1,10 @@
+package de.hfu;
+
+public class Util {
+
+    public static boolean istErstesHalbjahr(int monat) {
+        if ((monat < 1) || (monat > 12)) throw new IllegalArgumentException();
+        return monat <= 6;
+    }
+
+}
