7e4858222c5855cda76eac9611bae06e1c97c45f
Maximilian Maes <maximilian.maes@aesculap.de>
2021-12-02 12:20:43 +0100

    Added intergration tests Stub Test working

--- src/test/java/de/hfu/AppTest.java
+++ src/test/java/de/hfu/AppTest.java
@@ -1,6 +1,19 @@
 package de.hfu;
 
+import de.hfu.residents.domain.Resident;
+import de.hfu.residents.repository.ResidentRepository;
+import de.hfu.residents.service.BaseResidentService;
+import de.hfu.residents.service.ResidentServiceException;
 import org.junit.Test;
+
+import static org.easymock.EasyMock.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -21,7 +34,7 @@ public class AppTest {
      */
     @Test
     public void utilTest() {
-        assertTrue(Util.istErstesHalbjahr(5));
+        assertTrue(Util.istErstesHalbjahr(6));
         assertTrue(Util.istErstesHalbjahr(1));
         assertFalse(Util.istErstesHalbjahr(12));
         assertFalse(Util.istErstesHalbjahr(7));
@@ -87,4 +100,162 @@ public class AppTest {
 
     }
 
+    @Test
+    public void residentRepositoryStubTest() {
+        ResidentRepository stub = new ResidentRepository() {
+            @Override
+            public List<Resident> getResidents() {
+
+                List<Resident> residentList = new ArrayList<>();
+
+                residentList.add(new Resident("Max", "Mustermann", "Musterstrasse 1", "Musterbaum", new Date(2000, Calendar.JANUARY, 12)));
+                residentList.add(new Resident("Marlene", "Maier", "Musterstrasse 2", "Musterbaum", new Date(2000, Calendar.JANUARY, 12)));
+                residentList.add(new Resident("Mario", "Meyer", "Meyerstrasse 3", "Musterbaum", new Date(2000, Calendar.JANUARY, 12)));
+                residentList.add(new Resident("Dieter", "Kinzel", "Musterstrasse 4", "Musterbaum", new Date(2000, Calendar.JANUARY, 13)));
+
+
+                return residentList;
+            }
+        };
+
+        BaseResidentService residentService = new BaseResidentService();
+        residentService.setResidentRepository(stub);
+
+        Resident filterResident = new Resident("Ma*", "M*", null, null, null);
+
+        List l1 = residentService.getFilteredResidentsList(filterResident);
+
+        filterResident.setGivenName(null);
+        filterResident.setFamilyName(null);
+        filterResident.setStreet("Musterstrasse*");
+
+        List l2 = residentService.getFilteredResidentsList(filterResident);
+
+        filterResident.setStreet("M*");
+        List l3 = residentService.getFilteredResidentsList(filterResident);
+
+        assertEquals(3, l1.size());
+
+        assertEquals(3, l2.size());
+
+        assertEquals(4, l3.size());
+
+
+        try {
+            residentService.getUniqueResident(filterResident);
+            assert (false);
+        } catch (ResidentServiceException e) {
+        }
+
+        filterResident.setGivenName("Dieter");
+        filterResident.setStreet(null);
+        try {
+            residentService.getUniqueResident(filterResident);
+        } catch (ResidentServiceException e) {
+            fail();
+            e.printStackTrace();
+        }
+
+        filterResident.setFamilyName("Kinzel");
+        filterResident.setStreet("*");
+        try {
+            residentService.getUniqueResident(filterResident);
+            assert (false);
+        } catch (ResidentServiceException e) {
+        }
+
+        filterResident.setStreet("K*");
+        List l4 = residentService.getFilteredResidentsList(filterResident);
+
+        assertEquals(0, l4.size());
+
+        filterResident.setStreet(null);
+        filterResident.setDateOfBirth(new Date(2000, Calendar.JANUARY, 13));
+
+        try {
+            Resident resident = residentService.getUniqueResident(filterResident);
+            assertEquals("Dieter", resident.getGivenName());
+            assertEquals("Kinzel", resident.getFamilyName());
+            assertEquals("Musterstrasse 4", resident.getStreet());
+            assertEquals("Musterbaum", resident.getCity());
+        } catch (ResidentServiceException e) {
+            fail();
+            e.printStackTrace();
+        }
+
+    }
+
+    @Test
+    public void residentRepositoryMockTest() {
+
+        ResidentRepository mock = createMock(ResidentRepository.class);
+
+        BaseResidentService residentService = new BaseResidentService();
+        residentService.setResidentRepository(mock);
+
+        Resident filterResident = new Resident("Ma*", "M*", null, null, null);
+
+        List l1 = residentService.getFilteredResidentsList(filterResident);
+
+        filterResident.setGivenName(null);
+        filterResident.setFamilyName(null);
+        filterResident.setStreet("Musterstrasse*");
+
+        List l2 = residentService.getFilteredResidentsList(filterResident);
+
+        filterResident.setStreet("M*");
+        List l3 = residentService.getFilteredResidentsList(filterResident);
+
+        assertEquals(3, l1.size());
+
+        assertEquals(3, l2.size());
+
+        assertEquals(4, l3.size());
+
+
+        try {
+            residentService.getUniqueResident(filterResident);
+            assert (false);
+        } catch (ResidentServiceException e) {
+        }
+
+        filterResident.setGivenName("Dieter");
+        filterResident.setStreet(null);
+        try {
+            residentService.getUniqueResident(filterResident);
+        } catch (ResidentServiceException e) {
+            fail();
+            e.printStackTrace();
+        }
+
+        filterResident.setFamilyName("Kinzel");
+        filterResident.setStreet("*");
+        try {
+            residentService.getUniqueResident(filterResident);
+            assert (false);
+        } catch (ResidentServiceException e) {
+        }
+
+        filterResident.setStreet("K*");
+        List l4 = residentService.getFilteredResidentsList(filterResident);
+
+        assertEquals(0, l4.size());
+
+        filterResident.setStreet(null);
+        filterResident.setDateOfBirth(new Date(2000, Calendar.JANUARY, 13));
+
+        try {
+            Resident resident = residentService.getUniqueResident(filterResident);
+            assertEquals("Dieter", resident.getGivenName());
+            assertEquals("Kinzel", resident.getFamilyName());
+            assertEquals("Musterstrasse 4", resident.getStreet());
+            assertEquals("Musterbaum", resident.getCity());
+        } catch (ResidentServiceException e) {
+            fail();
+            e.printStackTrace();
+        }
+
+    }
+
 }
+
